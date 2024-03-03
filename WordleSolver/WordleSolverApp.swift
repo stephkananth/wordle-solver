995065a2035308f4e6052afe3ac1affca49a0968
Stephanie Ananth <steph.ananth@gmail.com>
2024-03-03 18:36:55 -0500

    Initial Commit

---
+++ WordleSolver/WordleSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleSolverApp.swift
+//  WordleSolver
+//
+//  Created by Steph on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WordleSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
